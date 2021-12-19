d707ad789593d5e1779e2390b449735e91f34963
krishna98967 <krishnavidya212@gmail.com>
2021-12-19 10:33:38 +0000

    added Igw for VPC

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,3 @@
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    
-}
-
-
 resource "aws_vpc" "default" {
     cidr_block = "${var.vpc_cidr}"
     enable_dns_hostnames = true
@@ -12,5 +5,11 @@ resource "aws_vpc" "default" {
         Name = "${var.vpc_name}"
 	Owner = "Sreeharsha Veerapalli"
 	environment = "${var.environment}"
+    }
+}
+resource "aws_internet_gateway" "default" {
+    vpc_id = "${aws_vpc.default.id}"
+	tags = {
+        Name = "${var.IGW_name}"
     }
 }
